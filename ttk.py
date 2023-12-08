a0d3bfc92203d210094f58bfbbd13415cb360c31
Sam X <eschxr@hotmail.com>
2023-12-08 11:13:08 +0800

    modifications made to satisfy video reqs

--- ttk.py
+++ ttk.py
@@ -8,19 +8,34 @@ class SearchBar(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        self.pack(padx=20, pady=(0, 20), fill=X)
+        self.pack(padx=20, pady=(0, 20), fill=BOTH, expand=True)
+        self.label_text = ttk.StringVar(value="No QUERY")
 
-        self.search_bar = ttk.Entry(self)
+        self.search_bar = ttk.Entry(self, font=("Garamond", 12))
         self.search_button = ttk.Button(self, text="Search", command=self.get_records)
+        self.results_label = ttk.Label(self, textvariable=self.label_text, font=("Garamond", 12), justify=CENTER)    
 
+        self.results_label.pack(expand=True, padx=20, pady=20, side=BOTTOM)
         self.search_bar.pack(side=LEFT, fill=X, expand=True)
         self.search_button.pack(side=LEFT, padx=(5, 0))
 
+
     def get_records(self):
         name = self.search_bar.get()
-        url_string = f"https://192.168.1.29:8000/names/{name}"
-        response = req.get(url_string).json()
-        print(response)
+        if len(name) < 1:
+            self.label_text.set(value="IMPROPER QUERY")
+        else:
+            url_string = f"https://192.168.1.29:8000/names/{name}"
+            response = req.get(url_string).json()
+            if "msg" in response:
+                self.label_text.set(value=response["msg"])
+            else:
+                response_string = f"""
+                Name: {response["name"]}
+                Age: {response["age"]}
+                Favorite Game: {response["favgame"]}
+                """
+                self.label_text.set(value=response_string)
 
 
 class ButtonGroup(ttk.Frame):
@@ -36,6 +51,32 @@ class ButtonGroup(ttk.Frame):
         self.cancel_button.pack(side=RIGHT)
 
 
+class GameSearchWindow(ttk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_title = ttk.Label(self, text="SMIC GAMING RECORDS", bootstyle=PRIMARY, font=("Garamond", 39, "bold"))
+        self.label_two = ttk.Label(self, text="A great tool for finding random SMIC students and their favorite games", bootstyle=SECONDARY, font=("Garamond", 20))
+
+        self.label_title.pack(padx=20, pady=(20, 10), anchor=W)
+        self.label_two.pack(padx=20, pady=(0, 20), anchor=W)
+        self.search_bar = SearchBar(self)
+
+
+class FoodSearchWindow(ttk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_title = ttk.Label(self, text="FOOD RECORDS", bootstyle=PRIMARY, font=("Garamond", 39, "bold"))
+        self.label_two = ttk.Label(self, text="A great tool for finding random food items and some cool facts/details", bootstyle=SECONDARY, font=("Garamond", 20))
+
+        self.label_title.pack(padx=20, pady=(20, 10), anchor=W)
+        self.label_two.pack(padx=20, pady=(0, 20), anchor=W)
+        self.search_bar = SearchBar(self)
+
+
 class App(ttk.Window):
 
     def __init__(self, theme):
@@ -44,13 +85,35 @@ class App(ttk.Window):
         self.title("SMIC RECORDS")
         self.geometry("1280x720")
 
-        self.label_title = ttk.Label(self, text="SMIC RECORDS", bootstyle=PRIMARY, font=("Garamond", 39, "bold"))
-        self.label_two = ttk.Label(self, text="A great tool for finding random foods completely unrelated to SMIC", bootstyle=SECONDARY, font=("Garamond", 20))
-        self.results_label = ttk.Label(self, text="No query given", font=("Garamond", 16))    
+        self.nav_frame = ttk.Frame(self, bootstyle=PRIMARY)
+        self.nav_frame.pack(fill=X, ipadx=5, ipady=5)
 
-        self.label_title.pack(padx=20, pady=(20, 10), anchor=W)
-        self.label_two.pack(padx=20, pady=(0, 20), anchor=W)
-        self.search_bar = SearchBar(self)
+        self.container = ttk.Frame(self)
+        self.container.pack(fill=BOTH, expand=True)
+
+        self.game_button = ttk.Button(self.nav_frame, text="Gaming Records", bootstyle=SECONDARY, command=lambda: self.change_frame("gaming"))
+        self.food_button = ttk.Button(self.nav_frame, text="Food Records", bootstyle=SECONDARY, command=lambda: self.change_frame("food"))
+
+        self.food_button.pack(side=RIGHT, padx=5)
+        self.game_button.pack(side=RIGHT, padx=5)
+
+        self.frames = {
+            "gaming": GameSearchWindow(self.container),
+            "food": FoodSearchWindow(self.container)
+        }
+        
+        self.current_frame = "gaming"
+
+    def set_frame(self):
+        self.frames[self.current_frame].pack(fill=BOTH, expand=True)
+
+    def remove_frame(self):
+        self.frames[self.current_frame].pack_forget()
+
+    def change_frame(self, name):
+        self.remove_frame()
+        self.current_frame = name
+        self.set_frame()
 
 
 if __name__ == "__main__":
